87604a811163283b52096c93201195bba971a66f
Peter Holak <peterholak@gmail.com>
2017-06-16 09:58:47 +0200

    update docker create.sh file

--- example/docker/create.sh
+++ example/docker/create.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-docker run -d --rm --name graphql-ws-kotlin -p 127.0.0.1:4567:4567 --memory=200m --cpu-period="100000" --cpu-quota="50000" -e JAVA_OPTS='-Xmx100m' graphql-ws-kotlin
-docker stop graphql-ws-kotlin
+docker run -d --rm \
+    --name graphql-ws-kotlin \
+    -p 127.0.0.1:4567:4567 \
+    --memory=200m --cpu-period="100000" --cpu-quota="50000" \
+    -e JAVA_OPTS='-Xmx100m' \
+    graphql-ws-kotlin
